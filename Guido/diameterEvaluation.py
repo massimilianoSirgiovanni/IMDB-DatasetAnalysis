52561047ff906fa077fd6012dda51ad11fb32efe
guidociardi <107542867+guidociardi@users.noreply.github.com>
2022-06-22 11:51:44 +0200

    Add files via upload

--- Guido/diameterEvaluation.py
+++ Guido/diameterEvaluation.py
@@ -1,16 +1,29 @@
 import networkx as nx
+from yearsFunctions import createGraphUpToYear
+import time
 
 
-def nodeGradeMax(graph):
+def diameterUpToYear(x, graph):
+    setYear = createGraphUpToYear(x, graph)
+    if len(setYear) == 0:
+        print(f"ERROR: There are no movies up to the year {x}")
+        return 0
+    maxGrade = nodeGradeMax(graph, setYear)
+    diam = diameter(graph, maxGrade)
+    return diam
+
+
+def nodeGradeMax(graph, set):
     maxN = -1  # Variable to store the id of the current
     sumMax = 0
-    giant = max(nx.connected_components(graph), key=len)
-
+    
+    
+    giant = max(nx.connected_components(graph.subgraph(set)), key=len)
+    
     for i in giant:
-        neighbors = list(graph.neighbors(i))
-        sumN = len(neighbors)
-        if sumN > sumMax:
-            sumMax = sumN
+        degreeNode = graph.degree(i)
+        if degreeNode > sumMax:
+            sumMax = degreeNode
             maxN = i
     if maxN == -1:
         print("ERROR: NO NODE FOUND IN THE GRAPH")
@@ -19,14 +32,9 @@ def nodeGradeMax(graph):
         return maxN
 
 
-
-
-
-def bfs(graph, startNode):
-    
+def bfs(graph, startNode, setNode):
     dizionarioEsterno = {}
-    
-    
+
     nodeDistance = {startNode: 0}
 
     nodeToVisit = [startNode]
@@ -38,59 +46,87 @@ def bfs(graph, startNode):
         neighbors = list(graph.neighbors(currentVert))
 
         for nbr in neighbors:
-            if nbr not in nodeDistance:
-                nodeToVisit.append(nbr)
-                nodeDistance[nbr] = nodeDistance[currentVert] + 1
-                if nodeDistance[nbr] not in dizionarioEsterno:    
-                    dizionarioEsterno[nodeDistance[nbr]] = [nbr]
-                else:
-                    dizionarioEsterno[nodeDistance[nbr]].append(nbr)
-                
-                if nodeDistance[nbr] > max:
-                    max = nodeDistance[nbr]
+            if nbr in setNode:
+                if nbr not in nodeDistance:
+                    nodeToVisit.append(nbr)
+                    nodeDistance[nbr] = nodeDistance[currentVert] + 1
+                    if nodeDistance[nbr] not in dizionarioEsterno:
+                        dizionarioEsterno[nodeDistance[nbr]] = [nbr]
+                    else:
+                        dizionarioEsterno[nodeDistance[nbr]].append(nbr)
+
+                    if nodeDistance[nbr] > max:
+                        max = nodeDistance[nbr]
         index = index + 1
+
 
     return (max, dizionarioEsterno)
 
 
-def Bi(graph, nodeList, lb):
-    
-    #maxEcc = (0,0)
+# Calcolo dell'eccentricità
+def eccentricity(graph, startNode, set):
+    max = bfs(graph, startNode, set)
+
+    return max
+
+
+#def Bi(graph, node, lb, level):
+def Bi(graph, nodeList, lb, set):
+    # maxEcc = (0,0)
     for i in nodeList:
-        temp = eccentricity(graph, i)
-        
-        if temp[0] > lb:
+        temp = eccentricity(graph, i, set)
+
+        #if temp[0] >= lb:
+        if temp[0] >= lb:#######era > e hai aggiunto =   if temp[0] >= lb:#######era > e hai aggiunto =
             return temp[0]
-    
+
     return lb
+    '''for j in graph.nodes:
+        distance = len(nx.shortest_path(graph, node, j))-1
+        if distance == level:
+            ecc = nx.eccentricity(graph, j)
+            if ecc > lb:
+                return ecc
+    return lb'''
 
 
 
-def diameter(graph, startNode):
-    
-    bfsTuple = eccentricity(graph, startNode)
-    i = bfsTuple[0]
+'''def diameter(graph, startNode):
+    ecc = nx.eccentricity(graph, startNode)
+    i = ecc
     lb = i
     ub = 2 * lb
-    
     while ub > lb:
-        bi = Bi(graph, bfsTuple[1][i], lb)
-        
-        if bi > 2*(i-1):
+        bi = Bi(graph, startNode, lb, i)
+        if bi > 2 * (i - 1):
             return bi
         else:
             lb = bi
-            ub = 2 * (i-1)
+            ub = 2 * (i - 1)
         i = i - 1
-        
+    return lb'''
+
+def diameter(graph, startNode, setNode):
+    start_time = time.time()
+    bfsTuple = eccentricity(graph, startNode, setNode)
+    end_time = time.time()
+    print(f"Compute bfsTuple TIME: {end_time - start_time}")
+    i = bfsTuple[0]
+    lb = i
+    ub = 2 * lb
+
+    while ub > lb:
+        start_time = time.time()
+        bi = Bi(graph, bfsTuple[1][i], lb, setNode)
+        end_time = time.time()
+        print(f"Bi() at {i} level TIME: {end_time - start_time}")
+
+
+        if bi > 2 * (i - 1):
+            return bi
+        else:
+            lb = bi
+            ub = 2 * (i - 1)
+        i = i - 1
+
     return lb
-        
-        
-    
-    
-
-# Calcolo dell'eccentricità
-def eccentricity(graph, startNode):
-    max = bfs(graph, startNode)
-
-    return max
--- Guido/yearsFunctions.py
+++ Guido/yearsFunctions.py
@@ -1,26 +1,47 @@
+import networkx as nx
+
 years = {
-    0: [],     # Errors in data
-    1870: [],   # (1870, 1880]
-    1880: [],   # (1880, 1890]
-    1890: [],   # (1890, 1900]
-    1900: [],   # (1890, 1910]
-    1910: [],   # (1910, 1920]
-    1920: [],   # (1920, 1930]
-    1930: [],   # (1930, 1940]
-    1940: [],   # (1940, 1950]
-    1950: [],   # (1950, 1960]
-    1960: [],   # (1960, 1970]
-    1970: [],   # (1970, 1980]
-    1980: [],   # (1980, 1990]
-    1990: [],   # (1990, 2000]
-    2000: [],   # (2000, 2010]
-    2010: [],   # (2010, 2020]
-    2020: []    # (2020, 2030]
+    0: set(),     # Errors in data
+    1870: set(),   # (1870, 1880]
+    1880: set(),   # (1880, 1890]
+    1890: set(),   # (1890, 1900]
+    1900: set(),   # (1890, 1910]
+    1910: set(),   # (1910, 1920]
+    1920: set(),   # (1920, 1930]
+    1930: set(),   # (1930, 1940]
+    1940: set(),   # (1940, 1950]
+    1950: set(),   # (1950, 1960]
+    1960: set(),   # (1960, 1970]
+    1970: set(),   # (1970, 1980]
+    1980: set(),   # (1980, 1990]
+    1990: set(),   # (1990, 2000]
+    2000: set(),   # (2000, 2010]
+    2010: set(),   # (2010, 2020]
+
+}
+
+yearsActor = {
+    0: set(),  # Errors in data
+    1870: set(),  # (1870, 1880]
+    1880: set(),  # (1880, 1890]
+    1890: set(),  # (1890, 1900]
+    1900: set(),  # (1890, 1910]
+    1910: set(),  # (1910, 1920]
+    1920: set(),  # (1920, 1930]
+    1930: set(),  # (1930, 1940]
+    1940: set(),  # (1940, 1950]
+    1950: set(),  # (1950, 1960]
+    1960: set(),  # (1960, 1970]
+    1970: set(),  # (1970, 1980]
+    1980: set(),  # (1980, 1990]
+    1990: set(),  # (1990, 2000]
+    2000: set(),  # (2000, 2010]
+    2010: set(),  # (2010, 2020]
 
 }
 
 def extractYear(movie):
-    for i in range(len(movie)-6, -1, -1):  # Start from the tail of the string because the year is always after the name
+    for i in range(len(movie)-5, -1, -1):  # Start from the tail of the string because the year is always after the name
         if movie[i] == "(" and (movie[i+1] == '2' or movie[i+1] == '1'): # Checking from the "(" to avoid situation like: (2003/II)
             year = int(movie[i+1] + movie[i+2] + movie[i+3] + movie[i+4])
             if movie[i+4] == "0":
@@ -31,14 +52,11 @@ def extractYear(movie):
 def meanForYear(x):
 
     x, sum, n = meanCheks(x)
-    
-    #print(years)
 
     while x > 1870:
         x = x - 10
         n = n + 10
-        sum = sum + len(years[x])  # L'anno x si considera inerente alla decade precedente
-                                   # (es. nel diz. years si ha che years[2000] contiene tutti i film nel range  ( 2000 , 2010 ]   )
+        sum = sum + len(years[x])
 
     return (sum, n, sum/n)  # NEL PROGETTO FINALE RESTITUIRà SOLO LA MEDIA (Terzo elemento)
 
@@ -47,12 +65,51 @@ def meanCheks(x):
         x = int(x)
 
     if x <= 1870:
-        return 0, 0, 1   # Non ci sono film prima del 1870
+        return 0, 0, 1
 
     x = x - (x % 10)
 
-    if x > 2030:
-        print("ERROR: You can not insert a year over 2030")
-        return 0, 0, 1   # Non avrebbe senso il controllo di una richiesta dei film futuri
+    if x >= 2030:
+        print("ERROR: You can not insert a year over 2020")
+        return 0, 0, 1
 
     return x, 0, 0
+
+
+'''def createGraphUpToYear(x, graph):
+    x = meanCheks(x)[0]
+    if x == 2020:
+        return graph
+    actorsNodes = {}
+    G = nx.Graph()
+    while x > 1870:
+        x = x - 10
+        movies = years[x]
+        for i in movies:
+            G.add_node(i)
+            addActorsFromMovie(i, actorsNodes, graph, G)
+    giant = max(nx.connected_components(G), key=len)
+    return graph.subgraph(giant)
+def addActorsFromMovie(movie, actorsDict, graph, newGraph):
+    for j in graph.neighbors(movie):
+        if j not in actorsDict:
+            actorsDict[j] = 0
+            newGraph.add_node(j)
+            newGraph.add_edge(movie, j)
+    return 0'''
+
+def createGraphUpToYear(x, graph):
+    x = meanCheks(x)[0]
+    #if x == 2020:
+    #    giant = max(nx.connected_components(graph), key=len)
+    #    return graph.subgraph(giant)
+    unionSet = set()
+    count = 0
+    while x > 1870:
+        x = x - 10
+        count = count + len(years[x]) + len(yearsActor[x])
+        unionSet = unionSet.union(years[x])
+        unionSet = unionSet.union(yearsActor[x])
+    print(count)
+
+    return unionSet
